ce47add54a8351501afeccbcdff946c551ab3f9e
wbarbosa <wbarbosa@artit.com.br>
2019-05-15 15:45:39 -0300

    Inserção de comentários úteis no cod.

--- app/src/main/java/com/wesley/tokengenerator/MainActivity.java
+++ app/src/main/java/com/wesley/tokengenerator/MainActivity.java
@@ -37,15 +37,16 @@ public class MainActivity extends AppCompatActivity{
     private TextView txtQRCodeReturn;
     private final Activity activity = this;
     private SharedPreferences sharedPreferences;
-    private static int offset = 10;
     private String key;
-    int delay = 0;   // delay de 1 seg.
-    int interval = 30000;  // intervalo de 1 seg.
-    Handler handler = new Handler();
     private Timer myTimer = new Timer();
     private ProgressBar mProgressBar;
     private CountDownTimer mCountDownTimer;
+
     private int prgss=0;
+    int delay = 0;   // delay nulo pra chamada do GenerateOtp.
+    int interval = 30000;  // intervalo de 30 segundos.
+
+    Handler handler = new Handler();
 
     //Carrega a Lib responsável pela chamada do método de geração do Hash
     static {
@@ -59,22 +60,26 @@ public class MainActivity extends AppCompatActivity{
         ActionBar bar = getSupportActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#144DC7")));
         setTitle("Token Generator");
+        //Shared Preferences utilizado para armazenar o QR Token
         sharedPreferences = getSharedPreferences("KEY-TOKEN", Context.MODE_PRIVATE);
+        //Inicialização dos componentes da UI
         initializeComponents();
         mProgressBar.setVisibility(View.INVISIBLE);
         key = sharedPreferences.getString("KEY-TOKEN", "empty");
+        //Verifica se existe um QR Token armazenado para iniciar a geração do OTP Code ou não.
         if(!key.equals("empty")){
             btnGetKey.setVisibility(View.INVISIBLE);
             btnGetKey.setEnabled(false);
             txtQRCodeReturn.setText("QR Token: " + key);
             mProgressBar.setVisibility(View.VISIBLE);
+            //Como existe um QR Token armazenado, já inicia a aplicação com a chamada temporizada da geração do OTP Code
             myTimer.scheduleAtFixedRate(new generateOtpToken(), delay, interval);
         }
     }
 
+    //Metodo responsável pelo incremento da barra que demonstra o tempo restante para atualização do OTP Code
     private void initCountDownTimer(){
         mCountDownTimer=new CountDownTimer(30000,1000) {
-
             @Override
             public void onTick(long millisUntilFinished) {
                 prgss++;
@@ -94,7 +99,6 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private void initializeComponents() {
-
         initCountDownTimer();
         txtQRCodeReturn = (TextView) findViewById(R.id.txtQRCodeReturn);
         mProgressBar=(ProgressBar)findViewById(R.id.progressbar);
@@ -115,6 +119,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 SharedPreferences.Editor editor = sharedPreferences.edit();
+                //Seta QR Token como vazio para forçar a exigência de nova leitura do QR Code
                 editor.putString("KEY-TOKEN","empty");
                 editor.apply();
                 btnGetKey.setEnabled(true);
@@ -129,9 +134,9 @@ public class MainActivity extends AppCompatActivity{
         txtCodeOTP = (TextView) findViewById(R.id.txtQRCode);
     }
 
+    //Retorno da leitura do QR Code
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
         if(result != null){
             if(result.getContents() != null){
@@ -149,6 +154,7 @@ public class MainActivity extends AppCompatActivity{
                     editor.apply();
                     btnGetKey.setEnabled(false);
                     btnGetKey.setVisibility(View.INVISIBLE);
+                    //Dispara a chamada do temporizador que gerencia o tempo de atualização do Código gerado
                     myTimer.scheduleAtFixedRate(new generateOtpToken(), delay, interval);
 
                 } catch (JSONException e) {
@@ -156,7 +162,7 @@ public class MainActivity extends AppCompatActivity{
                     Alert("Erro ao interpretar código!");
                 }
             }else{
-                Alert("Canceled!");
+                Alert("Leitura Cancelada!");
             }
         }else{
             super.onActivityResult(requestCode, resultCode, data);
@@ -164,21 +170,33 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private class generateOtpToken extends TimerTask  {
+        /**
+         * Como a utilização de Libs Nativas requer a interface entre linguagens diferentes,
+         * o que gera um determinado Overhead, esta sendo utilizado o conceito de paralelismo
+         * (nova thread) para gerenciar não só a temporização, mas também o processamento que
+         * envolve a chamada do método "generateOtp" da lib externa e os cálculos necessários
+         * para a conversão do Hash de 20 bytes para o Códido OTP final de 6 caracteres.
+         */
 
         public void run() {
             // colocar tarefas aqui ...
             byte[] byteArray = new byte[20];
+            //Chamada do metodo que gera o Hash de 20 bytes a partir do Token interpretado na leitura do QR Code.
             byteArray = generateOtp(key);
+            //pega os 4 bytes úteis para a decodificação.
             byte[] newArray = Arrays.copyOfRange(byteArray, 10, 14);
             String hexaValues = "";
             int valueInt = 0;
             for (int iterator = 0; iterator < newArray.length; iterator++) {
+                //Pega apenas o módulo do valor, desconsiderando possíveis valores negativos
                 valueInt = Math.abs(newArray[iterator]);
+                //"Mascara" feita para garantir o resultado de no máximo 31bits, truncando o primeiro bit (MSB).
                 if (iterator == 0) {
                     if (valueInt > 127) {
                         valueInt = valueInt - 128;
                     }
                 }
+                //Validação para que valores menores que 16 ainda sejam exibidos com 0 a esquerda (1 byte completo)
                 if (valueInt < 16) {
                     hexaValues = hexaValues + "0" + Integer.toHexString(valueInt);
                 } else
@@ -186,7 +204,9 @@ public class MainActivity extends AppCompatActivity{
             }
             int decimalCode = Integer.parseInt(hexaValues, 16);
             final String decimalCodeString = Integer.toString(decimalCode);
+            //Pega apenas os últimos 6 caracteres do código decimal gerado
             final String otpFinalCode = decimalCodeString.substring(decimalCodeString.length() - 6, decimalCodeString.length());
+            //Handler para a interação desta Thread com a View, na Thread principal
             handler.post(new Runnable() {
                 public void run() {
                     txtCodeOTP.setText(otpFinalCode);
@@ -199,7 +219,6 @@ public class MainActivity extends AppCompatActivity{
                 }
             });
         }
-
     }
 
     private void Alert(String msg){
@@ -207,8 +226,7 @@ public class MainActivity extends AppCompatActivity{
     }
 
     /**
-     * A native method that is implemented by the 'otpjni' library,
-     * which is packaged with this application.
+     * Método nativo implementado na lib "otpjni.cpp"
      */
     public native byte[] generateOtp(String key);
 }
